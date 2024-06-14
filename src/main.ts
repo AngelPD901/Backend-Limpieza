f0ec7df14a755b62ad36355ab135aba1b6eb8015
AngelPD901 <angelpuron0@gmai.com>
2024-06-14 08:15:56 +0200

    Frist Commit Doc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common'
-
+import {DocumentBuilder,SwaggerModule} from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('RESTFull API CleanApp')
+    .setDescription('Documentacion de la API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
   await app.listen(+process.env.PORT);
   console.log(`app listen in PORT: ${process.env.PORT}`);
 }
